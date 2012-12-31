08fc6fe8e8cdb9b85a2c8f3843ff63734dfc2d3b
Derbeth <derbeth@interia.pl>
2012-12-31 13:31:35 +0100

    split verse.rb into verse.rb and poem.rb

--- poem.rb
+++ poem.rb
@@ -1,74 +1,10 @@
 # -*- encoding: utf-8 -*-
 
-require './sentence'
 require './configuration'
-require './sentence_splitter'
-require './randomized_choice'
+require './sentence_manager'
+require './verse'
 
 require 'logger'
-
-class Verse
-	def initialize(sentence_mgr,conf)
-		splitter = SentenceSplitter.new(conf)
-		logger = conf.logger
-
-		sentences = []
-		sentences_text = []
-		last_subject = nil
-		while sentences_text.size < conf.lines_in_verse
-			sentence = nil
-			parts = nil
-			MAX_TRIES.times do
-				sentence = sentence_mgr.random_sentence
-				if last_subject && check_chance(conf.implicit_subject_chance)
-					sentence.implicit_subject = last_subject
-				end
-				text = sentence.write
-				parts = splitter.split(text)
-				if sentences_text.size + parts.size > conf.lines_in_verse
-					# cannot add two sentences, because it's the last sentence - try again
-					next
-				end
-				logger.debug "Split '#{parts.join(' | ')}'" if parts.size > 1
-				break
-			end
-			sentences << sentence
-			last_subject = sentence.subject
-			parts.each do |p|
-				sentences_text << p
-				break if sentences_text.size >= conf.lines_in_verse
-			end
-		end
-		sentences.each { |s| logger.debug "    #{s.debug_text}" }
-		logger.debug ""
-		@subject = find_subject(sentences)
-		@text = sentences_text.join("\n")
-	end
-
-	# gets subject (noun) representing the verse
-	def subject
-		@subject
-	end
-
-	def to_s
-		@text
-	end
-
-	private
-
-	include ChanceChecker
-
-	MAX_TRIES = 6
-
-	def find_subject(sentences)
-		subjects = []
-		sentences.each do |sentence|
-			subjects << sentence.subject if sentence.subject && sentence.subject.text != ''
-		end
-		return nil if subjects.empty?
-		subjects[rand(subjects.size)]
-	end
-end
 
 class Poem
 	def initialize(dictionary,grammar,sentence_mgr,conf)
@@ -88,7 +24,7 @@ class Poem
  5 ${VERB(11)} ${OBJ}
  4 ${VERB(2,IMP)} ${OBJ}
 10 ***
- 		END
+		END
 		title_sentence_mgr.read(title_sentences_defs)
 
 		title_sentence = title_sentence_mgr.random_sentence
