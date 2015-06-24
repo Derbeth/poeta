b0c447b3609e1e8f863dd5e2a5eaf1ba145e6d6c
Derbeth <derbeth@interia.pl>
2015-06-24 21:31:20 +0200

    Fix 'invalid byte sequence' on Ruby 1.9 with English locale

--- poeta.rb
+++ poeta.rb
@@ -1,5 +1,10 @@
 #!/usr/bin/ruby
 
+if RUBY_VERSION =~ /1.9/
+	Encoding.default_external = Encoding::UTF_8
+	Encoding.default_internal = Encoding::UTF_8
+end
+
 require 'optparse'
 
 require './poem'
